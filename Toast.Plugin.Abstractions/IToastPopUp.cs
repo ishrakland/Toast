2602cc218d944ae3ebdb7180e44a0a2fa3c36b7b
El andaloussi Ishrak <31075684+ishrakland@users.noreply.github.com>
2018-12-13 17:19:37 +0100

    Update IToastPopUp.cs

--- Toast.Plugin.Abstractions/IToastPopUp.cs
+++ Toast.Plugin.Abstractions/IToastPopUp.cs
@@ -5,12 +5,27 @@ namespace Plugin.Toast.Abstractions
     public interface IToastPopUp
     {
         /// <summary>
-        /// ShowToast display a Toast popup
+        /// ShowToastMessage display a Toast MESSAGE
         /// </summary>
         /// <param name="message">Message to display</param>
         void ShowToastMessage(string message);
+        
+        /// <summary>
+        /// ShowToastError display a Toast ERROR
+        /// </summary>
+        /// <param name="message">Message to display</param>
         void ShowToastError(string message);
+        
+        /// <summary>
+        /// ShowToastWarning display a Toast Warning
+        /// </summary>
+        /// <param name="message">Message to display</param>
         void ShowToastWarning(string message);
+        
+        /// <summary>
+        /// ShowToastSuccess display a Toast Success
+        /// </summary>
+        /// <param name="message">Message to display</param>
         void ShowToastSuccess(string message);
     }
 }
