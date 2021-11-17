82b86243b81616fe450b2ab9e5bae43607e73033
Jiří Matějka <jiri.matejka@webcom.cz>
2021-11-17 20:39:31 +0100

    To resolve crash on Android 11

--- Toast.Plugin.Android/ShowToastPopUp.cs
+++ Toast.Plugin.Android/ShowToastPopUp.cs
@@ -1,4 +1,5 @@
 ﻿using Android.Graphics;
+using Android.OS;
 using Android.Views;
 using Android.Widget;
 using Plugin.Toast.Abstractions;
@@ -74,13 +75,19 @@ namespace Plugin.Toast
             // To dismiss existing toast, otherwise, the screen will be populated with it if the user do so
             _instance?.Cancel();
             _instance = Android.Widget.Toast.MakeText(Android.App.Application.Context, message, length);
-            View tView = _instance.View;
-            if (!string.IsNullOrEmpty(backgroundHexColor))
-                tView.Background.SetColorFilter(Color.ParseColor(backgroundHexColor), PorterDuff.Mode.SrcIn);//Gets the actual oval background of the Toast then sets the color filter
 
-            TextView text = (TextView)tView.FindViewById(Android.Resource.Id.Message);
-            if (!string.IsNullOrEmpty(textHexColor))
-                text.SetTextColor(Color.ParseColor(textHexColor));
+            if (Android.OS.Build.VERSION.SdkInt < BuildVersionCodes.R)
+            {
+                View tView = _instance.View;
+                if (!string.IsNullOrEmpty(backgroundHexColor))
+                    tView.Background.SetColorFilter(Color.ParseColor(backgroundHexColor),
+                        PorterDuff.Mode
+                            .SrcIn); //Gets the actual oval background of the Toast then sets the color filter
+
+                TextView text = (TextView) tView.FindViewById(Android.Resource.Id.Message);
+                if (!string.IsNullOrEmpty(textHexColor))
+                    text.SetTextColor(Color.ParseColor(textHexColor));
+            }
             _instance.Show();
         }
 
